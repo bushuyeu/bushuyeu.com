45940ceb259f60cf6cca76c01492135bbc999f0c
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-02-26 14:31:35 -0500

    delete (c)

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,6 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Pavel Bushuyeu.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
